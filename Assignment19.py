a99e927a7a6124d7de84cbacbd575d84e62f6cca
ra30012001 <113654718+ra30012001@users.noreply.github.com>
2022-11-28 03:35:19 +0800

    Update Assignment19.py

--- Assignment19.py
+++ Assignment19.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import LabelEncoder
